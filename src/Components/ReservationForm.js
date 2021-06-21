715596ae9463328ad06bee5e8a831242c73e01aa
duvivierbri <duvivierbri@gmail.com>
2021-06-21 17:22:31 -0400

    Finished with the form, the CSS isn't final, we can come up with color schemes/a design towards the end

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -12,10 +12,6 @@ class ReservationForm extends Component{
         notes:''
     }
 
-    componentDidUpdate(){
-        
-    }
-
     //Uses name in the form to update name state
     updateName = (event) =>{
         event.preventDefault();
@@ -35,9 +31,13 @@ class ReservationForm extends Component{
     //Uses date in form to update date state
     updateDate = (event) =>{
         event.preventDefault();
+        let month = document.getElementById("monthInput").value;
+        let day = document.getElementById("dayInput").value;
+        let dateInput = month + ", " + day;
+
         this.setState({
-            date: event.target.value
-        });
+            date: dateInput
+        })
     }
 
     //Uses time in form to update time state
@@ -69,8 +69,26 @@ class ReservationForm extends Component{
         });
     }
 
+    //Send info to database function
+    addReservation = async(newReservation) => {
+        axios.post('http://localhost:4000/reservations', newReservation)
+        .then((response) => console.log(response.data))
+        .catch((error) => console.log(error))
+ 
+        //Reset values to prepare for another input
+        {this.setState({
+             name: '',
+             numberOfPeople:'',
+             date:'',
+             time:'',
+             allergies:'',
+             notes:''
+        })}
+     }
+
     //Called when the user submits the form (with button)
-    onSubmit = () =>{  
+    onSubmit = (event) =>{  
+        event.preventDefault();
         //Creates new reservation with the information given to be sent to database
         const newReservation = {
             name: this.state.name,
@@ -81,55 +99,56 @@ class ReservationForm extends Component{
             notes: this.state.notes
         }
 
-       //this.addReservation(newReservation);
-    }
-
-    testSubmit = (event) =>{
-        event.preventDefault();
-
-        const newReservation = {
-            name: this.state.name,
-            numberOfPeople: this.state.numberOfPeople,
-            date: this.state.date,
-            time: this.state.time,
-            allergies: this.state.allergies,
-            notes: this.state.notes
-        }
-        
-        console.log(newReservation);
+       this.addReservation(newReservation);
+       console.log(newReservation)
     }
     
-    //Send info to database
-    addReservation = async(newReservation) => {
-       axios.post('http://localhost:3000/APIHERE', newReservation)
-       .then((response) => console.log(response.data))
-       .catch((error) => console.log(error))
-
-       //Reset values 
-       {this.setState({
-            name: '',
-            numberOfPeople:'',
-            date:'',
-            time:'',
-            allergies:'',
-            notes:''
-       })}
-    }
     
     render(){
         return (
             <div className="reservationForm">
-                <form onSubmit = {this.testSubmit}>
-                    <p>New Reservation</p>
+                <h1 className="reservationTitle">New Reservation</h1>
+                <p>
+                    Thank you for choosing to reserve a table at our restaurant! We promise to make you feel welcome and at home :)
+                    Please fill out the information below, and we'll reserve a spot for you at one of our tables!
+                </p>
 
+                <form onSubmit = {this.onSubmit}>
                     <input type="text" onChange = {this.updateName} placeholder = "Name" required/>
                     <br/>
 
                     <input type="text" onChange = {this.updateNumPeople} placeholder = "Number of People Attending" required/>
                     <br/>
 
-                    <input type="text" onChange = {this.updateDate} placeholder = "Date" required/>
-                    <br/>
+                    <div className="dateSelector">
+                        <div className="dateTool">
+                            <select onChange={this.updateDate} id="monthInput" required>
+                                <option value="">Month</option>
+                                <option value="January">January</option>
+                                <option value="February">February</option>
+                                <option value="March">March</option>
+                                <option value="April">April</option>
+                                <option value="May">May</option>
+                                <option value="June">June</option>
+                                <option value="July">July</option>
+                                <option value="August">August</option>
+                                <option value="September">September</option>
+                                <option value="October">October</option>
+                                <option value="November">November</option>
+                                <option value="December">December</option>
+                            </select>
+                        </div>
+                        <div className="dateTool">
+                            <select onChange={this.updateDate} id="dayInput" required>
+                                <option value="">Day</option>
+                                <option value="05">05</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="20">20</option>
+                                <option value="25">25</option>
+                            </select>
+                        </div>
+                    </div>
 
                     <div className="timeSelecter">
                         <div className="timeTool">
