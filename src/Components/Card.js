da1b575bfdc372cbfff96e8b701c884084c69382
duvivierbri <duvivierbri@gmail.com>
2021-06-23 15:15:25 -0400

    Some tweaks to details alert box

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,6 +2,12 @@ import React, { Component } from "react"
 import ReactDOM from 'react-dom';
 import axios from "axios";
 import { Modal } from "@material-ui/core";
+import { Dialog } from "@material-ui/core";
+import { DialogTitle } from "@material-ui/core";
+import { DialogActions } from "@material-ui/core";
+import { DialogContent } from "@material-ui/core";
+import { DialogContentText } from "@material-ui/core";
+import { Slide } from "@material-ui/core";
 
 //Each Reservation will be displayed in one of these cards on the "See Reservations" Page
 class ReservationCard extends React.Component{
@@ -24,13 +30,13 @@ class ReservationCard extends React.Component{
             email: this.props.reservation.email
         }
 
-        console.log(reservationObject)
+        console.log("This email: " + this.props.reservation.email)
 
         //Delete the item from the database and refresh the page!
         //Refreshing should repopulate the array with the updated data
         axios.delete('http://localhost:4000/reservations/' + this.props.reservation.name )
         .then(
-            window.location.reload()
+            //window.location.reload()
         )
     }
 
@@ -39,7 +45,7 @@ class ReservationCard extends React.Component{
             <div className="reservationCard">
                 <div className="resCardFeature">
                     <div className="nameAndSchedule">
-                        <h3 className = "cardName">Name: {this.props.reservation.name}</h3>
+                        <h3 className = "cardName">Name: {this.props.reservation.email}</h3>
                         <p className = "cardDateandTime">Scheduled For: {this.props.reservation.date}, {this.props.reservation.time}</p>
                     </div>
                 </div>
@@ -51,18 +57,25 @@ class ReservationCard extends React.Component{
                 <div className="resCardFeature">
                     <button className="deleteButton" onClick={this.deleteReservation}>Delete</button>
                 </div>
+                
+                <Dialog open={this.state.showDetails}>
+                    <DialogTitle id="alert-dialog-slide-title">{this.props.reservation.name}</DialogTitle>
+                    
+                    <DialogContent>
+                        <DialogContentText>
+                            <p>Date of Reservation: {this.props.reservation.date}</p>
+                            <p>Time of Reservation: {this.props.reservation.time}</p>
+                            <p>Number of People: {this.props.reservation.numberOfPeople}</p>
+                            <p>Allergies: {this.props.reservation.allergies}</p>
+                            <p>Email: {this.props.reservation.email}</p>
+                            <p>Extra Notes: {this.props.reservation.notes}</p>
+                        </DialogContentText>
+                    </DialogContent>
 
-                <Modal className="detailModal" open = {this.state.showDetails}>
-                    <div>
-                        <h1>Name: {this.props.reservation.name}</h1>
-                        <p>Date of Reservation: {this.props.reservation.date}</p>
-                        <p>Time of Reservation: {this.props.reservation.time}</p>
-                        <p>Number of People: {this.props.reservation.numberOfPeople}</p>
-                        <p>Allergies: {this.props.reservation.allergies}</p>
-                        <p>Email: {this.props.reservation.email}</p>
-                        <p>Extra Notes: {this.props.reservation.notes}</p>
-                    </div>
-                </Modal>
+                    <DialogActions>
+                        <button onClick={() => this.setState({showDetails: false})}>Close</button>
+                    </DialogActions>
+                </Dialog>
             </div>
             
         )
