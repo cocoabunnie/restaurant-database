5dae141f8c975abfd6c85d0eba940bd48e626e9d
ChrisAndrade <chriskevinandrade@gmail.com>
2021-06-23 10:46:51 -0400

    Name Change

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/reservations', require('./restaurant_api'));
+router.use('/reservations', require('./reservation_api'));
 
 module.exports = router;
